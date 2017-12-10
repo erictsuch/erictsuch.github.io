782fa2830ea1f68bddfd28d595cb4a5bc13db431
erictsuch <erictsuchiyama@gmail.com>
2017-12-10 10:46:14 -0500

    Week 4 Homework + Final Project Proposal

---
+++ class-work/first-javascript/script.js
@@ -0,0 +1,23 @@
+
+var product = 10 * 10;
+var quotient = 100 / 10;
+var difference = 50 - 17;
+var sum = 50 + 17;
+var name = 'Eric' + ' Tsuchiyama';
+
+console.log(product + quotient + difference, sum, name);
+
+function onra () {
+    var city = "Saigon";
+
+    console.log(`Last Tango In ${city}`);
+    
+}
+
+onra();
+onra();
+onra();
+
+var hello = document.getElementById("hello");
+
+hello.style.color = 'tomato';
