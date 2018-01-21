7cf172e778372f3617ef8f866649dd481236fdc2
erictsuch <erictsuchiyama@gmail.com>
2018-01-21 12:14:27 -0500

    Week 8 homework + Final Pseudo Code

---
+++ final-project/js/app.js
@@ -0,0 +1,5 @@
+// slideToggle menu
+// lazy load images
+// click on image opens child window with larger image and details on pricing
+
+// I realize there isn't much here, but I don't think I need much more than this (though I might realize more as I'm building it out). 
